7e66627479eccd42c2b2ef7d2f5223a34ffda8f5
Gabriel Gomes <gabrielgomesdeveloper1999@gmail.com>
2024-09-14 17:30:21 +0100

    Organizing files and stuff

--- index.js
+++ index.js
@@ -1,64 +1,33 @@
 import cardArray from "./data.js";
 
-
-
-
-
-
-
-
-let cards = [];
-
-
-
-
-
-
-const cardsPosition = [];
-
 cardArray.sort(() => 0.5 - Math.random());
+const grid = document.querySelector("#grid");
 
-console.log(cardArray[0]);
+console.log(grid);
 
-const grid = document.getElementById("grid");
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
 
-for (let i = 0; i < 12; i++) {
-  const card = document.createElement("img");
-  card.className = "card";
-  card.src = "images/blank.png";
-  card.setAttribute("data-id", i);
-  card.addEventListener("click", flipCard);
-  grid.appendChild(card);
+
+    card.onclick = flipCard
+
+
+    card.className = "card";
+    card.setAttribute("data-id", i)
+
+    card.src = "images/blank.png";
+
+    grid.appendChild(card);
+  }
 }
+
+createBoard();
+
 
 function flipCard() {
-  const id = this.dataset.id;
-
-  this.src = cardArray[id].img;
-
-  cards.push(cardArray[id].name);
-  cardsPosition.push(id);
-
-  if (cards.length === 2) {
-    viewGameStatus();
-  }
+  console.log(this.dataset.id);
+  
 }
 
-function viewGameStatus() {
-  const images = document.querySelectorAll("img");
-
-  console.log(images[cardsPosition[0]]);
-  console.log(images[cardsPosition[1]]);
-
-  //console.log(images[cardsPosition[0]]);
-
-  if (cards[0].name === cards[1].name) {
-
-    images[cardsPosition[0]].src = "images/white.png";
-    images[cardsPosition[1]].src = "images/white.png";
-  } else {
-    cards = []
-    images[cardsPosition[0]].src = "images/blank.png";
-    images[cardsPosition[1]].src = "images/blank.png";
-  }
-}
+console.log(cardArray);
